303d5f8478f618b3409b75021ad2de8d19c798ed
Sai Nishwanth <sainishwanthraj@gmail.com>
2021-12-31 20:44:17 +0530

    First Commit

---
+++ Flipkart.py
@@ -0,0 +1,43 @@
+from bs4 import  BeautifulSoup
+import requests
+from playsound import playsound
+
+import os
+import time
+
+header = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"}
+
+def price(page, soup,URL):
+    page = requests.get(URL, headers = header)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    try:
+        price = soup.find("div", class_ = "_30jeq3 _16Jk6d").get_text().strip()
+        price = price.replace(',','')
+        return int(price[1:])
+    except:
+        price(page,soup,URL)
+def main():
+    URL = input("Enter the Link: ")
+    while True:
+        page = requests.get(URL, headers = header)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        try:
+            title = soup.find("span", class_ = "B_NuCI").get_text().strip()
+            break
+        except:
+            pass
+    print(title+"\n")
+    price_og = price(page,soup,URL)
+    print(f"Price - {price_og}")
+    price_limit = int(input("Enter the Price Threshold: "))
+    sound = input("Enter path to your music: ")
+    while True:
+        if(price_limit >= price(page,soup,URL)):
+            playsound(sound)
+            break
+        else:
+            continue
+if __name__ == "__main__":
+    main()
+
+
---
+++ README.md
@@ -0,0 +1,11 @@
+# Flipkart Price Tracker with python
+
+## Uses
+        Play's Music when the price of the item drops below your given threshold
+
+## Requirements:
+        Python3 
+        BeautifulSoup
+        Requests
+        Playsound
+## Installation
