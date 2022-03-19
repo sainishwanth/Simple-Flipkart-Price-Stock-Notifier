b1857e6134c54e796aaff27cba936cde46521f69
Sai Nishwanth <sainishwanthraj@gmail.com>
2022-03-19 12:11:54 +0530

    Coments

--- Flipkart.py
+++ Flipkart.py
@@ -10,13 +10,13 @@ header = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWe
 path1 = os.getcwd()
 URL = input("Enter the URL: ")
 
-class item():
+class item():  #Class which contains methods and attributes of the product
     def __init__(self,URL,page,soup):
         self.page = page
         self.URL = URL
         self.soup = soup
     
-    def price(self):
+    def price(self): #Method to retrieve the price of the item
         self.page = requests.get(URL, headers = header)
         self.soup = BeautifulSoup(self.page.content, 'html.parser')
         try:
@@ -26,7 +26,7 @@ class item():
         except:
             pass
     
-    def stock(self):
+    def stock(self):  #Method to check if the item is in stock
         self.page = requests.get(URL, headers = header)
         self.soup = BeautifulSoup(self.page.content, 'html.parser')
         try:
@@ -36,15 +36,15 @@ class item():
             return None
         
     @staticmethod
-    def music(path1):
+    def music(path1):  #Method to Play Music. (Not an attribute of Product but welp)
         try:
-            path.exists(path1 + '/alarm.wav')
+            path.exists(path1 + '/alarm.wav')    #Unix Convention of file management
             path1 = path1 + '/alarm.wav'
         except:
-            path.exists(path1 + '\alarm.wav')
-            path1 = path1 + '\alarm.wav'
-        webbrowser.open(URL)
-        playsound(path1)
+            path.exists(path1 + '\alarm.wav')    #Windows Convention of file Managament
+            path1 = path1 + '\alarm.wav'      
+        webbrowser.open(URL)                     #Opening the Product Page 
+        playsound(path1)                         #Playing the "Music" i.e Alarm when triggered
             
 
 def main():
@@ -64,14 +64,14 @@ def main():
         price_limit = int(input("Enter the Price Threshold: "))
         while True:
             
-            if price_limit >= product.price():
+            if price_limit >= product.price(): #Loop that keeps checking for if the price has fallen below the price set by the user
                 product.music(path1)
             else:
                 time.sleep(10)
                 print("Running..")
                 continue
             break
-    elif inpt == 2:
+    elif inpt == 2:                            #Loop for checking if the item is in stock
         while True:
             if product.stock() is  None:
                 product.music(path1)
